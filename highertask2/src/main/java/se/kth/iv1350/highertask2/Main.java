b81c817bd495386129383387b0f9b9228c5bd38a
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-30 17:34:36 +0200

    a

--- highertask2/src/main/java/se/kth/iv1350/highertask2/Main.java
+++ highertask2/src/main/java/se/kth/iv1350/highertask2/Main.java
@@ -17,12 +17,20 @@ public final class Main {
         RandomComposedChar rComposedChar = new RandomComposedChar();
         RandomInheritedChar rInChar = new RandomInheritedChar();
        
-        System.out.println(rInString.nextString(5));
-        System.out.println(rCompString.nextString(5));
-        System.out.println(rInString.nextString(5));
-        System.out.println(rCompString.nextString(5));
+        System.out.println("We have adapted java.util.random class.");
+        System.out.println("One class for random chars and one for random strings.");
+        System.out.println("Only random string is shown as it depends on random char");
+        System.out.println("");
+
+        System.out.println("Random String class outputs with random seed: ");
+        System.out.println("Adapted using inheritance: "+rInString.nextString(5));
+        System.out.println("Adapted using compsiton:   "+rCompString.nextString(5));
+        System.out.println("Adapted using compsiton:   "+rInString.nextString(5));
+        System.out.println("Adapted using inheritance: "+rCompString.nextString(5));
         rInString.setSeed(5);
         rCompString.setSeed(5);
+
+        System.out.println("Random String class outputs with random seed: ");
         System.out.println(rInString.nextString(5));
         System.out.println(rCompString.nextString(5));
         System.out.println(rInString.nextString(10));
