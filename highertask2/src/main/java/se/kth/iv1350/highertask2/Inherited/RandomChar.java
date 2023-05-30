4f6fc3b2d586db8d1a13072d2411c22efe2c26c1
KTH-Kista-BatuhanNur <bhnur@kth.se>
2023-05-30 16:11:22 +0200

    a

--- highertask2/src/main/java/se/kth/iv1350/highertask2/Inherited/RandomChar.java
+++ highertask2/src/main/java/se/kth/iv1350/highertask2/Inherited/RandomChar.java
@@ -4,4 +4,28 @@ import java.util.Random;
 
 public class RandomChar extends Random {
     
+
+    public  RandomChar() {
+        super();
+    }
+
+    public RandomChar(long seed) {
+        super(seed); 
+    }
+
+
+    protected char NextChar(char origin, char bound){
+        int originInt = (int) origin; 
+        int boundInt = (int) bound; 
+        int zero = 0;
+
+        if(originInt < bound){
+            int n =  boundInt-origin;
+            if (n>0){
+                return (char) (nextInt(n)+origin);
+            }
+                }
+            return (char) zero;
+
+    }
 }
